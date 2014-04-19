878be245ee6eba477059ccd33c74b65eed7d9aa9
Jacob Fan <jacob.fan@gmail.com>
2014-04-19 23:56:39 +0800

    update test

--- search_demo/tests/test.py
+++ search_demo/tests/test.py
@@ -29,6 +29,7 @@ def api_access(path, params, body=None):
 
 
 def test(function, expected_result, amount=50, *args):
+    print "Testing:", function.__name__, args
     import time
     t1 = time.time()
     for i in range(amount):
@@ -71,9 +72,16 @@ def recommend(type, params):
     res = api_access("public/recommender/", params)
     return res
 
+def events(event_type, params):
+    params["api_key"] = api_key
+    params["event_type"] = event_type
+    res = api_access("public/events/", params)
+    return res
+
+
 test(post_items, {"code": 0}, 1)
-#test(post_search, {"code": 0}, 5)
-#test(post_suggest, {"code": 0}, 5)
+test(post_search, lambda x:x["errors"]=={}, 5)
+test(post_suggest, lambda x:x["errors"]=={}, 5)
 
 test(recommend, lambda x:x["code"]==0, 1, "AlsoViewed", {"user_id": "U1", "item_id": "I1", "amount": 5})
 test(recommend, lambda x:x["code"]==0, 1, "ByBrowsingHistory", {"user_id": "U1", "amount": 5})
@@ -82,3 +90,10 @@ test(recommend, lambda x:x["code"]==0, 1, "BoughtTogether", {"user_id": "U1", "i
 test(recommend, lambda x:x["code"]==0, 1, "UltimatelyBought", {"user_id": "U1", "item_id": "I1", "amount": 5})
 test(recommend, lambda x:x["code"]==0, 1, "ByPurchasingHistory", {"user_id": "U1", "amount": 5})
 test(recommend, lambda x:x["code"]==0, 1, "ByShoppingCart", {"user_id": "U1", "amount": 5})
+
+test(events, {"code": 0}, 1, "ViewItem", {"user_id": "U1", "item_id": "I1", "amount": 5})
+test(events, {"code": 0}, 1, "AddFavorite", {"user_id": "U1", "item_id": "I1", "amount": 5})
+test(events, {"code": 0}, 1, "RemoveFavorite", {"user_id": "U1", "item_id": "I1", "amount": 5})
+test(events, {"code": 0}, 1, "RateItem", {"user_id": "U1", "item_id": "I1", "score": 3, "amount": 5})
+test(events, {"code": 0}, 1, "AddOrderItem", {"user_id": "U1", "item_id": "I1", "amount": 5})
+test(events, {"code": 0}, 1, "RemoveOrderItem", {"user_id": "U1", "item_id": "I1", "amount": 5})
