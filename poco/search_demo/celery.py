29ddd19cc36ae5745445d037c48c9b0b3b212a91
Sun Liwen <sunliwen@gmail.com>
2014-05-16 03:22:24 +0800

    Refactor project

--- poco/search_demo/celery.py
+++ poco/search_demo/celery.py
@@ -11,7 +11,7 @@ from django.conf import settings
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'search_demo.settings')
 
-app = Celery('search_demo')
+app = Celery('poco')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
@@ -22,4 +22,3 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
-
