70f21612d7f3b12af54bbe37d1be3c8153910caf
root <root@en83.intranet>
2014-06-03 22:10:51 +0800

    fixed reset_items script

--- poco/scripts/reset_items.py
+++ poco/scripts/reset_items.py
@@ -3,9 +3,7 @@ from common.site_manage_utils import reset_items
 
 
 def run(site_id):
-    print "This script is outdated. Try create_site instead."
-    sys.exit(1)
-    answer = raw_input("Do you really want to reset items of site(All items would lost!): '%s'?(enter 'yes' to continue)" % site_id)
+    answer = raw_input("Do you really want to reset items of site(All items in mongodb/ES would lost!): '%s'?(enter 'yes' to continue)" % site_id)
     if answer == "yes":
         reset_items(site_id)
     else:
