1ed91b85cb71118cf0353ecdf9959a146f69df8a
lisztli <sockpuppet.lea@gmail.com>
2014-12-27 23:22:47 +0800

    refs #106

--- poco/services/batch/preprocessing/backfiller.py
+++ poco/services/batch/preprocessing/backfiller.py
@@ -1,8 +1,9 @@
-import sys
+import os
 import time
 from datetime import datetime
 from datetime import timedelta
 import logging
+import tempfile
 from common import utils
 import json
 
@@ -18,7 +19,7 @@ class BackFiller:
             last_dt is the time point to start calculation
             hours is the max hours older then last_dt of the records
     """
-    def __init__(self, connection, site_id, last_ts, output_file_path):
+    def __init__(self, connection, site_id, last_ts, output_file_path, cached_result_path):
         self.connection = connection
         self.site_id = site_id
         self.raw_logs = utils.getSiteDBCollection(
@@ -27,6 +28,7 @@ class BackFiller:
             connection, site_id, "tmp_user_identified_logs_plo")
         self.last_ts = last_ts
         self.output_file_path = output_file_path
+        self.cached_result_path = cached_result_path
         self.ptm_id2user = {}
 
     def dateTimeAsFloat(self, datetime):
@@ -58,11 +60,59 @@ class BackFiller:
                             log_doc, "ANO_" + log_doc["ptm_id"])
                 else:
                     self._updateFilledUserId(log_doc, user_id)
-                self.raw_logs.save(log_doc)
-            del log_doc["_id"]
+                if log_doc.has_key('_id'):
+                    self.raw_logs.save(log_doc)
+            if log_doc.has_key('_id'):
+                del log_doc["_id"]
             log_doc["created_on"] = self.dateTimeAsFloat(log_doc["created_on"])
-            self.f_output.write("%s\n" % json.dumps(log_doc))
+            rst = "%s\n" % json.dumps(log_doc)
+            self.f_output.write(rst)
             self.f_output.flush()
+            return rst
+
+    def doc_generator(self, begin, end):
+        """
+        yield the doc and raw log line
+        """
+
+        # 1. get the latest data in cache
+        cf = None
+        begin_ts = time.mktime(begin.utctimetuple())
+        end_ts = time.mktime(begin.utctimetuple())
+        latest_line = None
+        latest_doc = None
+        if os.path.isfile(self.cached_result_path):
+            cf = open(self.cached_result_path)
+            latest_line = cf.readline()
+            try:
+                latest_doc = json.loads(latest_line)
+                begin_ts = latest_doc['created_on']
+            except:
+                pass
+        begin = datetime.fromtimestamp(begin_ts)
+
+        # 2. yield the new data in mongo
+        cursor = self.raw_logs.find({'created_on': {'$gt': begin, '$lt': end}}, timeout=False).sort("created_on", -1)
+        for log_doc in cursor:
+            yield log_doc
+        del cursor
+        # 3. yield valid data in cache
+        if cf and latest_doc:
+            latest_doc['created_on'] = datetime.fromtimestamp(latest_doc['created_on'])
+            yield latest_doc
+            for ln in cf.readlines():
+                try:
+                    ln_doc = json.loads(ln)
+                    if ln_doc['created_on'] > end_ts:
+                        ln_doc['created_on'] = datetime.fromtimestamp(ln_doc['created_on'])
+                        yield ln_doc
+                    else:
+                        break
+                except:
+                    pass
+        if cf:
+            cf.close()
+
 
     # TODO: start a cursor every 200000 entries?
     # We use find(timeout=False) here and use "del cursor" to close it.
@@ -87,12 +137,16 @@ class BackFiller:
         # end = datetime.strptime('2013-12-01T00:00:00.0Z', '%Y-%m-%dT%H:%M:%S.0Z')
         # cursor = self.raw_logs.find({'created_on': {'$gte': begin, '$lt': end}}, timeout=False).sort("created_on", -1).limit(10000000)
 
-        end = datetime.utcnow()
+        # I found the online log server isn't using utc
+        # end = datetime.utcnow()
+        end = datetime.now()
         begin = end - timedelta(days=30)  # should be configurable
-        cursor = self.raw_logs.find({'created_on': {'$gte': begin, '$lt': end}}, timeout=False).sort("created_on", -1)
+
+        cached_dir = os.path.dirname(self.cached_result_path)
+        tmp_file = tempfile.NamedTemporaryFile(dir=cached_dir, delete=False)
 
         try:
-            for log_doc in cursor:
+            for log_doc in self.doc_generator(begin, end):
                 count += 1
                 if count % 10000 == 0:
                     logger.info("Count: %s, %s rows/sec" %
@@ -103,11 +157,16 @@ class BackFiller:
                     is_old_region = True
                 if latest_ts_this_time is None:
                     latest_ts_this_time = log_doc["created_on"]
-                self.workOnDoc(log_doc, is_old_region)
+                log = self.workOnDoc(log_doc, is_old_region)
+                tmp_file.write(log)
             if latest_ts_this_time is not None:
                 return latest_ts_this_time
             else:
                 return self.last_ts
         finally:
             self.f_output.close()
-            del cursor
+            tmp_file_path = tmp_file.name
+            tmp_file.close()
+            if os.path.isfile(self.cached_result_path):
+                os.remove(self.cached_result_path)
+            os.rename(tmp_file_path, self.cached_result_path)
--- poco/services/batch/settings.py
+++ poco/services/batch/settings.py
@@ -3,9 +3,11 @@ tac_command = "tac"
 mongodb_host = "localhost"
 replica_set = None
 log_dir = None
+cached_result_dir = None
 
 from local_settings import *
 
 assert work_dir != None
 assert mongodb_host is not None
 assert log_dir is not None
+assert cached_result_dir is not None
