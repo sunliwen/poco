482e9a230a2c43128aa0b196c680e7c292f274f6
lisztli <sockpuppet.lea@gmail.com>
2014-12-27 17:48:15 +0800

    fixes #58

--- poco/crons/erase_batch_log.sh
+++ poco/crons/erase_batch_log.sh
@@ -1,7 +1,9 @@
 # delete the temp files on batch server later than x days
 # this script should be invoked at 3:00 AM everyday
 
-LOGPATH=/cube/service/batch/work_dir/vegaga
+# the logpath should be ``work_dir`` variable in services/batch/local_settings.py, plus SITE_ID
+# You should set the correct value depends on your deploy
+LOGPATH=/cube/data/poco/batch/work_dir/haoyaoshi
 DAYS=7
 
 find $LOGPATH -type d -mmin +$((60*24*$DAYS)) | xargs rm -rf
